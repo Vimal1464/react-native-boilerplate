ac2e22d0442c74b37285cc4b648dddda84ee224b
Vimal1464 <vimalpandey1978.78@gmail.com>
2023-12-31 10:51:35 +0530

    Loader

--- src/screens/Demo.tsx
+++ src/screens/Demo.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import Background from '../../src/components/Background';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import AntDesign from 'react-native-vector-icons/AntDesign';
@@ -9,12 +9,13 @@ import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityI
 import CustomButton from '../../src/components/CustomButton';
 import IconTextButton from '../../src/components/IconTextButton';
 import IconButton from '../components/IconButton';
-import {LogBox, SafeAreaView, ScrollView, View} from 'react-native';
+import {LogBox, SafeAreaView, ScrollView, Text, View} from 'react-native';
 import Header from '../components/Header';
 import appColors from '../constants/colors';
 import DropDown from '../components/DropDown';
-import { NavigationProp, useNavigation } from '@react-navigation/native';
-const genderArray=[
+import {NavigationProp, useNavigation} from '@react-navigation/native';
+import Loading from '../components/Loading';
+const genderArray = [
   {
     id: 1,
     label: 'Male',
@@ -23,37 +24,55 @@ const genderArray=[
     id: 2,
     label: 'Female',
   },
-]
+];
 const monthsArray = [
-  { id: 1, month: "January" },
-  { id: 2, month: "February" },
-  { id: 3, month: "March" },
-  { id: 4, month: "April" },
-  { id: 5, month: "May" },
-  { id: 6, month: "June" },
-  { id: 7, month: "July" },
-  { id: 8, month: "August" },
-  { id: 9, month: "September" },
-  { id: 10, month: "October" },
-  { id: 11, month: "November" },
-  { id: 12, month: "December" }
+  {id: 1, month: 'January'},
+  {id: 2, month: 'February'},
+  {id: 3, month: 'March'},
+  {id: 4, month: 'April'},
+  {id: 5, month: 'May'},
+  {id: 6, month: 'June'},
+  {id: 7, month: 'July'},
+  {id: 8, month: 'August'},
+  {id: 9, month: 'September'},
+  {id: 10, month: 'October'},
+  {id: 11, month: 'November'},
+  {id: 12, month: 'December'},
 ];
 
 const Demo = () => {
-  LogBox.ignoreLogs([" WARN  Sending `onAnimatedValueUpdate` with no listeners registered"]) // for ios when we navigate back 
+  LogBox.ignoreLogs([
+    ' WARN  Sending `onAnimatedValueUpdate` with no listeners registered',
+  ]); // for ios when we navigate back
   const navigation = useNavigation<any>();
   const [dropDownValue1, setDropdownValue1] = useState<any>(); // type depend on your dropdown data
   const [dropDownValue2, setDropdownValue2] = useState<any>(); // type depend on your dropdown data
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    const loadTrigger = setTimeout(() => {
+      // setLoading(false)
+    }, 2000);
+
+    return () => {
+      clearTimeout(loadTrigger);
+    };
+  }, []);
+  if (loading) {
+    return (
+      <Loading
+        message='Custom Loader'
+      />
+    );
+  }
   return (
     <Background>
       <SafeAreaView style={{flex: 1, backgroundColor: 'transparent'}}>
         <ScrollView showsVerticalScrollIndicator={false} bounces={false}>
           <Header text="Custom DropDown" />
           <DropDown
-          labelTextStyle={{
-            color:"white"
-          }}
-
+            labelTextStyle={{
+              color: 'white',
+            }}
             dropDownIconColor={'white'}
             bannerStyle={{
               backgroundColor: appColors.primary,
@@ -65,12 +84,11 @@ const Demo = () => {
             access="label"
             label="Custom Dropdown"
             placeHolder="Select Value"
-
           />
-            <DropDown
-          labelTextStyle={{
-            color:"white"
-          }}
+          <DropDown
+            labelTextStyle={{
+              color: 'white',
+            }}
             dropDownIconColor={'white'}
             bannerStyle={{
               backgroundColor: appColors.primary,
@@ -130,7 +148,7 @@ const Demo = () => {
                 );
               }}
             />
-             <IconButton
+            <IconButton
               buttonStyle={{backgroundColor: 'purple'}}
               showLoader
               isLoader
@@ -166,7 +184,6 @@ const Demo = () => {
                 );
               }}
             />
-           
           </ScrollView>
           <ScrollView
             overScrollMode="auto"
@@ -291,7 +308,7 @@ const Demo = () => {
               showLoader
               buttonStyle={{
                 backgroundColor: appColors.thinBlue,
-                padding:15
+                padding: 15,
               }}
               textStyle={{color: 'black'}}
               isLoader
@@ -301,14 +318,13 @@ const Demo = () => {
               }}
               title="Custom Icon Button With Loader"
             />
-             <CustomButton
-          title='Next Page for More..'
-              onPress={()=>{
-                navigation.navigate("More")
+            <CustomButton
+              title="Next Page for More.."
+              onPress={() => {
+                navigation.navigate('More');
               }}
-          />
+            />
           </View>
-         
         </ScrollView>
       </SafeAreaView>
     </Background>
